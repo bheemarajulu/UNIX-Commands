2e62f9e26f221eb50b0cb3435abc7f822908fad9
Rajulu <bheemarajulu90@gmail.com>
2015-09-28 06:36:33 -0400

    Update code1.c

--- Week1/code1.c
+++ Week1/code1.c
@@ -12,3 +12,13 @@ Now compile,
 $ gcc -o hello hello.c
 $ ./hello
 output: Hello World
+
+
+
+// Here (./) tells the shell to compile from current directory.
+// By default a.out that assembler output is placed in default executable path if file is executed without -o
+// including files are located in /usr/local/bin and local network files are located in usr/local/bin or/opt
+// for gnu libraries /usr/lib/gcc/
+// for include libraries /usr/include, for non standard places include -I flag for include
+// $ gcc -I/usr/openwin/include fred.c 
+
